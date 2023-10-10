38caaa461fc0b542b00bb1b3641aa6fcb33c8b42
Jornay <lucasdeafg@yahoo.com.br>
2023-10-10 11:52:16 -0300

    feat: first class

--- src/main/java/jornay/com/todoList/todoListApplication.java
+++ src/main/java/jornay/com/todoList/todoListApplication.java
@@ -1,13 +1,13 @@
-package jornay.com.demo;
+package jornay.com.todoList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class todoListApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(todoListApplication.class, args);
 	}
 
 }
---
+++ src/main/java/jornay/com/todoList/user/UserModel.java
@@ -0,0 +1,7 @@
+package jornay.com.todoList.user;
+
+public class UserModel {
+    public String username;
+    public String name;
+    public String password;
+}
